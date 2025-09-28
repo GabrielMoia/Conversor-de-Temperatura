028de09101d9ccb4cb27151df47170b8488bb160
GabrielMoia <gabrielriosmoia@gmail.com>
2025-09-28 14:10:27 -0300

    Add files via upload

---
+++ Temperatura.py
@@ -0,0 +1,11 @@
+### Conversor de Temperatura de Fahrenheit para Celsius ###
+
+print("Conversor de Temperatura de Fahrenheit para Celsius")
+
+Fahrenheit = float(input("Digite a Temperatura em Fahrenheit: "))
+
+Celsius = (Fahrenheit - 32)/1.8
+
+Celsius = (Fahrenheit - 32)/1.8
+
+print(f"A Temperatura de hoje em Celsius é: {Celsius:.2f}°")
